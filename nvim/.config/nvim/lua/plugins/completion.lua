ad72c4cad2b62ad7ac50e8e3a3a05e131276adc6
Victor He <victorhe97@gmail.com>
2026-09-01 22:39:30 -0400

    Add tab to accept autocomplete in nvim

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
   version = "*",
   opts = {
-    keymap = { preset = "default" },
+    keymap = { preset = "super-tab" },
     appearance = { nerd_font_variant = "mono" },
     sources = { default = { "lsp", "path", "snippets", "buffer" } },
     completion = { documentation = { auto_show = true } },
